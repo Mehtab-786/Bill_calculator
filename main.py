384ff70a9882a27e4a0c6766a0ea718af6bca629
Mehtab <150262610+Mehtab-786@users.noreply.github.com>
2023-11-11 19:35:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-print("Welcome to Tip calculator")
+print("Welcome to Tip calculator\n")
+
 bill = int(input("What was your total bill : "))
 tip = int(input("What percentage you would like to give as tip : "))
 people = int(input("How many guys are there : "))
-each_persons = ((bill * (tip / 100)) + bill) / people
-print(f"Each person will get {each_persons} in their share")
+
+share = ((bill * (tip / 100)) + bill) / people
+
+# "%.2f"  is used for only 2 number after decimal points
+
+print(f"Each person will get ${'%.2f' % share} in their share")
